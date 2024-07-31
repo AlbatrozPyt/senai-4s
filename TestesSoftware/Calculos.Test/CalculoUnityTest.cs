662044785ccade55df844f4a652ae0b046a4fa62
Matheus Enrike Ramalho Ortiz <24004134870@info.local>
2024-07-31 17:22:52 -0300

    Testes unitarios

---
+++ TestesSoftware/Calculos.Test/CalculoUnityTest.cs
@@ -0,0 +1,72 @@
+﻿using Xunit;
+
+namespace Calculos.Test
+{
+    public class CalculoUnityTest
+    {
+        [Fact]
+        public void SomarDoisNumeros()
+        {
+            var n1 = 5.5;
+            var n2 = 1.5;
+
+            var valorEsperado = 7;
+
+            var soma = Calculo.Somar(n1, n2);
+
+            Assert.Equal(valorEsperado, soma);
+        }
+
+
+        [Fact]
+        public void SubtrairDoisNumeros()
+        {
+            double n1 = 17;
+            double n2 = 10;
+
+            var valorEsperado = 7;
+
+            var subtracao = Calculo.Subtrair(n1, n2);
+
+            Assert.Equal(valorEsperado, subtracao);
+        }
+
+        [Fact]
+        public void MultiplicarDoisNumeros()
+        {
+            double n1 = 3.5;
+            double n2 = 2;
+
+            var valorEsperado = 7;
+
+            var multiplicar = Calculo.Multiplicar(n1, n2);
+
+            Assert.Equal(valorEsperado, multiplicar);
+        }
+
+        [Fact]
+        public void DividirDoisNumeros() 
+        {
+            double n1 = 14;
+            double n2 = 2;
+
+            var valorEsperado = 7;
+
+            var dividir = Calculo.Dividir(n1, n2);
+
+            Assert.Equal(valorEsperado, dividir);
+        }
+
+        [Fact]
+        public void ModuloNumero()
+        {
+            double n1 = -7;
+
+            var valorEsperado = 7;
+
+            var modular = Calculo.Modular(n1);
+
+            Assert.Equal(valorEsperado, modular);
+        }
+    }
+}
---
+++ TestesSoftware/Calculos/Calculo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos
+{
+    public static class Calculo
+    {
+        public static double Somar(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static double Subtrair(double x, double y)
+        {
+            return (x - y);
+        }
+
+        public static double Multiplicar(double x, double y)
+        {
+            return x * y;
+        }
+
+        public static double Dividir(double x, double y)
+        {
+            return x / y;
+        }
+
+        public static double Modular(double x)
+        {
+            return x < 0 ? x * -1 : x;
+        }
+
+    }
+}
